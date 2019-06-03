a29dde451586cde7dd3a12c4d86fecb23bb22d8c
liyanmin <13824774439@163.com>
2019-06-03 15:32:20 +0800

    生产者发送消息-异步方式

--- src/main/java/com/kafka/producer/demo/asyn/MyAsynProducer.java
+++ src/main/java/com/kafka/producer/demo/asyn/MyAsynProducer.java
@@ -1,28 +1,38 @@
-package com.kafka.producer.demo;
+package com.kafka.producer.demo.asyn;
 
-
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 import java.util.concurrent.Future;
 
-public class MyProducer {
-
+public class MyAsynProducer {
     public static void main(String[] args) {
         Properties kafkaProperties = new Properties();
         kafkaProperties.put("bootstrap.servers", "192.168.8.137:9092");
         kafkaProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        kafkaProperties.put("request.timeout.ms", "160000");
         KafkaProducer producer = new KafkaProducer(kafkaProperties);
-        ProducerRecord record = new ProducerRecord("test", "name", "water");
+        ProducerRecord record = new ProducerRecord("test", "name2", "water asyn");
         try {
-            Future result = producer.send(record);
             //need to call get method to send msg
-            System.out.println(result.get());
+            producer.send(record, new ProducerCallback()).get();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private static class ProducerCallback implements Callback{
+
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+            System.out.println("finish");
+            if(exception != null){
+                //need to log the exception
+                exception.printStackTrace();
+            }
+        }
+    }
+
 }
