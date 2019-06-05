8bb644cc6d028435952d27ec0ca355e5cf7ef1ce
liyanmin <13824774439@163.com>
2019-06-05 15:18:11 +0800

    消费者消费信息 - 多线程方式 & 与spring boot的测试blog

---
+++ src/main/java/com/kafka/consumer/demo/executor/MyCustomerWithExecutor.java
@@ -0,0 +1,6 @@
+package com.kafka.consumer.demo.executor;
+
+public class MyCustomerWithExecutor {
+    //todo 使用多线程操控customer，请参考confluent官网博客：https://www.confluent.io/blog/tutorial-getting-started-with-the-new-apache-kafka-0-9-consumer-client/
+    //todo kafka & spring boot测试集成：https://blog.mimacom.com/testing-apache-kafka-with-spring-boot/
+}
